1af5c1ccb9e61b8d644f26847bfffccc8ca37502
Daniel Toloto <dctoloto@gmail.com>
2021-03-28 16:21:49 -0300

    fix: url regex validation

--- src/validations/team/index.tsx
+++ src/validations/team/index.tsx
@@ -1,9 +1,10 @@
 import * as Yup from 'yup';
 
+// eslint-disable-next-line no-useless-escape
+const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
 export const teamSchema = Yup.object().shape({
   name: Yup.string().required(),
-  website: Yup.string()
-    .matches(/^(http|https):\/\/[^ "]+$/)
-    .required(),
+  website: Yup.string().matches(expression).required(),
   squad: Yup.array().length(11).required(),
 });
